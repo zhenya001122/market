f3c50ffba31b3e1e6e5ee11b6644dd856ab5d684
Evgeniy Shutko <sluckltu@gmail.com>
2022-09-12 17:47:18 +0300

    add users

--- market/settings.py
+++ market/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'market.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "market",
+       "USER": "market",
+       "PASSWORD": "market",
+       "HOST": "localhost",
+       "PORT": 5432,
+   }
 }
+
 
 
 # Password validation
